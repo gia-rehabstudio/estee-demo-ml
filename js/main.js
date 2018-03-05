6d4373d88af2e6b00e4eeb2e31d9db2640dbd1b8
gi <gisel.armando@rehabstudio.com>
2018-03-05 00:53:25 +0000

    estee demo initial commit

---
+++ js/main.js
@@ -0,0 +1,93 @@
+'use strict';
+
+// Put variables in global scope to make them available to the browser console.
+var video = document.querySelector('video');
+var canvas = window.canvas = document.querySelector('canvas');
+canvas.width = 480;
+canvas.height = 360;
+
+var button = document.querySelector('button');
+
+const container = document.getElementById('container');
+
+const take = document.getElementById('take');
+const takeagain = document.getElementById('take-again');
+
+take.onclick = function() {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+
+  canvas.getContext('2d').
+    drawImage(video, 0, 0, canvas.width, canvas.height);
+
+  const rawImgData = canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, '');
+
+  container.className="still";
+
+  predict('estee', rawImgData)
+    .then(bestMatch => this.setState({
+      bestMatch
+  }))
+
+};
+
+takeagain.onclick = function() {
+  container.className = "play";
+}
+
+var constraints = {
+  audio: false,
+  video: true
+};
+
+function handleSuccess(stream) {
+  window.stream = stream; // make stream available to browser console
+  video.srcObject = stream;
+}
+
+function handleError(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+function toPercentage(real) {
+  return Math.round(real * 100);
+}
+
+var source   = document.getElementById("entry-template").innerHTML;
+var template = Handlebars.compile(source);
+
+var productsource = document.getElementById("product").innerHTML;
+var productTemplate = Handlebars.compile(productsource);
+
+navigator.mediaDevices.getUserMedia(constraints).
+    then(handleSuccess).catch(handleError);
+
+//////  Clarifai  ////////
+
+const app = new Clarifai.App({
+  apiKey: 'f7ca76c0a1c2401ea7168971e71c12eb'
+});
+
+function predict(model, imgUrl) {
+  return app.models.predict(model, imgUrl)
+    .then(res => {
+      const bestMatch = res.rawData.outputs[0].data.concepts[0].name;
+      const bestValue = res.rawData.outputs[0].data.concepts[0].value;
+
+      let percentage = toPercentage(bestValue);
+
+      let outputDiv = document.getElementById('results');
+      let output = template({percentage: percentage});
+
+      if (bestMatch in products) {
+        // get values from products list.
+        let product = products[bestMatch];
+        // add the percentage
+        product.percentage = percentage;
+        output += productTemplate(product);
+      }
+
+      outputDiv.innerHTML = output;
+
+    });
+}
---
+++ js/products.js
@@ -0,0 +1,23 @@
+const products = {
+
+	"Advanced Night Repair": {
+		title: "Advanced Night Repair Synchronized Recovery Complex II",
+		description: "Advanced Night Repair is our number one serum. It's suitable for all skin types and dramatically reduces the look of all key signs of aging. And it'll help you achieve your skin goal of glowing skin.<br/>Apply to clean skin before you moisturize. Apply a drop on each fingertip of one hand. Touch fingertips of both hands together. Massage serum over face and neck, starting at the center, moving outward.",
+		buylink: "https://www.esteelauder.co.uk/product/681/26959/product-catalog/skincare/advanced-night-repair/synchronized-recovery-complex-ii",
+		assistant: "https://www.esteelauder.co.uk/product/681/39367/product-catalog/skincare/advanced-night/micro-cleansing-foam"
+	},
+
+	"Advanced Night Micro Cleansing Foam": {
+		title: "Advanced night micro cleansing foam",
+		description: "This conditioning formula transforms into a soft, airy foam that removes makeup and impurities, including pollution, as it purifies deep within skin's surface to improve your overall healthy look. The first step to your keep looking younger skincare routine.",
+		buylink: "https://www.esteelauder.co.uk/product/681/39367/product-catalog/skincare/advanced-night/micro-cleansing-foam",
+		assistant: "https://www.esteelauder.co.uk/product/681/39367/product-catalog/skincare/advanced-night/micro-cleansing-foam"
+	},
+	"human face": {
+		title: "What a lovely human face!",
+		description: "Make it look as radiant as possible with Estée Lauder",
+		buylink: "https://www.esteelauder.co.uk/product/681/26959/product-catalog/skincare/advanced-night-repair/synchronized-recovery-complex-ii",
+		assistant: "https://www.esteelauder.co.uk/product/681/39367/product-catalog/skincare/advanced-night/micro-cleansing-foam"
+	}
+
+};
